7af6231d4acc025e8883ed4e45d4ce99fa47530e
DevyTech <bawotongdevaldy@gmail.com>
2025-09-07 18:43:32 +0800

    awidjaoi

--- app/src/main/java/com/example/japaneselearningapp/database/SplashActivity.java
+++ app/src/main/java/com/example/japaneselearningapp/database/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.example.japaneselearningapp.database;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +28,15 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String versionStr = "Version "+pInfo.versionName;
+            TextView version = findViewById(R.id.tvVersion);
+            version.setText(versionStr);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
